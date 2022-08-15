cfea6cf7fca7747ab20e8fc5abbff1cbbc5ab430
IgorKlimovich <85802281+IgorKlimovich@users.noreply.github.com>
2022-08-15 22:06:20 +0300

    innitial commit

--- Salesforce/force-app/main/default/aura/caseDuplicates/caseDuplicatesController.js
+++ Salesforce/force-app/main/default/aura/caseDuplicates/caseDuplicatesController.js
@@ -1,17 +1,21 @@
 ({
-    doInit : function(component, event, helper) {
+    handleChange :function(component, event, helper){
+        var options = event.getParam('value');
         var recordId=  component.get("v.recordId");
         var action = component.get("c.getDuplicatesCases");
-        action.setParams({ "caseId": recordId});
+        action.setParams({ "caseId": recordId,"options": options});
         action.setCallback(this, function(response){
             var state = response.getState();
             if (state === "SUCCESS") {
                 if(response.getReturnValue().length===0){
                     component.set("v.isEmptyCases", true);
                 }
+                else{
+                    component.set("v.isEmptyCases", false);
+                }
                 component.set("v.cases", response.getReturnValue());
             }
         });
-        $A.enqueueAction(action);
+        $A.enqueueAction(action)
     }
 })
